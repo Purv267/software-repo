120c979e8f72613101ebf471958990cf62109c39
Purv267 <patpurvr@sheridancollege.ca>
2020-06-24 10:01:27 -0400

    committed

---
+++ src/Practice.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author purvp
+ */
+public class Practice {
+    public int purv;
+    public double shubh;
+
+    public int getPurv() {
+        return purv;
+    }
+
+    public void setPurv(int purv) {
+        this.purv = purv;
+    }
+
+    public double getShubh() {
+        return shubh;
+    }
+
+    public void setShubh(double shubh) {
+        this.shubh = shubh;
+    }
+    
+}
